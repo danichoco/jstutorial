547241140499886c6705c6972fd1ccef9335cc85
daniela <danielavento25@gmail.com>
2022-04-27 16:05:34 -0400

    modificando el codigo

--- main.js
+++ main.js
@@ -3,23 +3,29 @@
 
 function contar(maxcount){
    
-    for ( number = 1; number<=maxcount; number=number+1){
-    console.log(number);
+    for ( n = 1; n<=maxcount; n++){
+    console.log(n);
+  }
 }
-}
-contar(1000);
+
 console.log('........');
-contar(10);
-console.log('.........');
-contar(80);
-/*
+console.log('aritmetica');
  console.log(math.sumar(2,3 ));
 console.log(math.restar(30,3 ));
 console.log(math.multiplicar(20, 40 ));
 console.log(math.dividir(100,10 ));
 
+console.log('........');
+console.log('valor absoluto');
 console.log(math.abs(-100)); //debe devolver 100
 console.log(math.abs( 400)); //debe devolver 400
 console.log(math.abs(20)); //debe devolver 20
 console.log(math.abs(-10)); //debe devolver 10
-*/
+
+console.log('........');
+console.log('contando numeros enteros');
+contar(1000);
+console.log('........');
+contar(10);
+console.log('.........');
+contar(80);
